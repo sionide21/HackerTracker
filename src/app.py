239c1213224914b6994cfde81555382d9a42e944
Ben Olive <sionide21@gmail.com>
2014-01-01 16:12:56 -0500

    Some html

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, session
+from flask import Flask, request, abort, session, render_template
 from hackertracker.database import Session, Model
 from hackertracker.event import Event
 from sqlalchemy import create_engine
@@ -11,15 +11,20 @@ Session.configure(bind=create_engine(os.environ['DATABASE_URL'], echo=True))
 
 @app.route('/track/<thing>', methods=['GET', 'POST'])
 def track(thing):
-    thing = thing.replace('_', ' ')
-    event = Event.for_name(thing)
+    event = Event.for_name(thing.replace('_', ' '))
     event.track(attrs=request.form)
     return ""
 
 
+@app.route('/event/<thing>', methods=['GET', 'POST'])
+def event(thing):
+    event = Event.for_name(thing.replace('_', ' '))
+    return render_template("event.html", event=event)
+
+
 @app.route('/')
 def index():
-    return "<br/>".join(e.name for e in Event.all())
+    return render_template("index.html", events=Event.all())
 
 
 @app.before_request
